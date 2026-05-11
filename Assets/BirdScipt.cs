c67b60e15c9fd61a8e96d1dc5975daa2d1a9e597
Yuvraj Prasad <yuvrajprasadknk797@gmail.com>
2026-05-11 17:32:27 +0530

    Updating repo
    Uploading all the files

---
+++ Assets/BirdScipt.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScipt : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapstrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
+        {
+            myRigidbody.velocity = Vector2.up * flapstrength;
+        }
+        
+        if(transform.position.y > 20) 
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
